436e3d00e529e2ee88fb6db89d2a14f8db274370
ThomasNugent <tomnugent98@gmail.com>
2018-10-11 17:06:18 +1000

    Added camInput which captures an image from a XIMEA cam and stores it

---
+++ camInput.m
@@ -0,0 +1,22 @@
+%% Camera input. Attach the XIMEA cam and then run this code to get a
+% single image from it saved to variable "im".
+vid = videoinput('gentl', 1, 'BGRA8Packed');
+src = getselectedsource(vid);
+
+vid.FramesPerTrigger = 1;
+
+triggerconfig(vid, 'manual');
+triggerconfig(vid, 'immediate');
+
+src.ExposureTime = 35000;
+src.WhiteBalanceBlue = 1;
+src.WhiteBalanceGreen = 1;
+src.WhiteBalanceRed = 1;
+start(vid);
+ 
+im = getdata(vid);
+imshow(im)
+%% TODO
+% Do image processing (top down transform, undistorted, imbinarize,
+% iblobs to identify objects and their centroid+area, do motion planning
+% then finally convert config space (in pixels) to IK pose angles
