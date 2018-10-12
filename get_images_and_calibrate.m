2f3e2cecdbe3baf95e795b253c3774d00d403dc9
ThomasNugent <tomnugent98@gmail.com>
2018-10-12 11:32:45 +1000

    Finished untested calibrate function

--- get_images_and_calibrate.m
+++ get_images_and_calibrate.m
@@ -1,15 +1,45 @@
 function [cameraParams, imagesUsed, estimationErrors, undistortedImage] ...
-    = get_images_and_calibrate()
+    = get_images_and_calibrate(checkerSize)
 %GET_IMAGES_AND_CALIBRATE Captures 20 images of a checkerboard and
 %calibrates the camera
 
 % See Elliot's top down camera implementation
 for i = 1:20
     im = get_image()
-    imagesUsed(i) = im
+    images(i) = im
 end
 
-% Now calibrate
+[imagePoints, boardSize, imagesUsed] = ...
+    detectCheckerboardPoints(images);
+
+% Read the first image to obtain image size
+originalImage = imread(images(1));
+[mrows, ncols, ~] = size(originalImage);
+
+% Generate world coordinates of the corners of the squares
+squareSize = checkerSize;  % in units of 'millimeters'
+worldPoints = generateCheckerboardPoints(boardSize, squareSize);
+
+% Calibrate the camera
+[cameraParams, imagesUsed, estimationErrors] = ...
+    estimateCameraParameters(imagePoints, worldPoints, ...
+    'EstimateSkew', false, 'EstimateTangentialDistortion', false, ...
+    'NumRadialDistortionCoefficients', 2, 'WorldUnits', 'millimeters', ...
+    'InitialIntrinsicMatrix', [], 'InitialRadialDistortion', [], ...
+    'ImageSize', [mrows, ncols]);
+
+% View reprojection errors
+h1=figure; showReprojectionErrors(cameraParams);
+
+% Visualize pattern locations
+h2=figure; showExtrinsics(cameraParams, 'CameraCentric');
+
+% Display parameter estimation errors
+displayErrors(estimationErrors, cameraParams);
+
+% For example, you can use the calibration data to remove effects of 
+% lens distortion.
+undistortedImage = undistortImage(originalImage, cameraParams);
 
 end
 
